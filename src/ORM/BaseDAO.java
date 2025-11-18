f781f5b5c40f77d9587f9ac372e3e8af7cc5368f
Francesco Cinotti <129069101+Cinotti02@users.noreply.github.com>
2025-11-18 16:18:35 +0100

    add BaseDAO and getConnection method to the project

---
+++ src/ORM/BaseDAO.java
@@ -0,0 +1,17 @@
+package ORM;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class BaseDAO {
+
+    protected Connection getConnection() throws SQLException {
+        return DBConnection.getInstance().getConnection();
+    }
+
+    // Metodi comuni a tutti i DAO (estendibili in futuro)
+    protected void checkId(int id) {
+        if (id <= 0)
+            throw new IllegalArgumentException("ID must be positive");
+    }
+}
