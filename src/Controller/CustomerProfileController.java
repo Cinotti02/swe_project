0b54e767d6b8f602e0f4d1ffa1acc070ce1947a9
Francesco Cinotti <129069101+Cinotti02@users.noreply.github.com>
2025-11-19 15:05:31 +0100

    Create CustomerProfileController: controller for customer profile actions (view, update, email change, fidelity points)

--- src/Controller/CustomerProfileController.java
+++ src/Controller/CustomerProfileController.java
@@ -1,4 +1,55 @@
 package Controller;
 
+import DomainModel.user.User;
+import ServiceLayer.ProfileService;
+
+import java.sql.SQLException;
+
 public class CustomerProfileController {
+
+    private final ProfileService profileService;
+
+    public CustomerProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    public void showProfile(int userId) {
+        try {
+            User user = profileService.getProfile(userId);
+            System.out.println("=== Profilo ===");
+            System.out.println("Username: " + user.getUsername());
+            System.out.println("Email: " + user.getEmailValue());
+            System.out.println("Nome: " + user.getName());
+            System.out.println("Cognome: " + user.getSurname());
+            System.out.println("Fidelity points: " + user.getFidelityPoints());
+        } catch (SQLException e) {
+            System.err.println("Impossibile caricare il profilo: " + e.getMessage());
+        }
+    }
+
+    public void updateProfile(User user, String username, String name, String surname) {
+        try {
+            profileService.updatePersonalInfo(user, username, name, surname);
+            System.out.println("Profilo aggiornato");
+        } catch (SQLException | IllegalArgumentException e) {
+            System.err.println("Errore durante l'aggiornamento: " + e.getMessage());
+        }
+    }
+
+    public void changeEmail(User user, String email) {
+        try {
+            profileService.updateEmail(user, email);
+            System.out.println("Email aggiornata");
+        } catch (SQLException | IllegalArgumentException e) {
+            System.err.println("Impossibile aggiornare l'email: " + e.getMessage());
+        }
+    }
+
+    public void addFidelityPoints(User user, int points) {
+        try {
+            profileService.addFidelityPoints(user, points);
+        } catch (SQLException e) {
+            System.err.println("Impossibile aggiornare i punti: " + e.getMessage());
+        }
+    }
 }
